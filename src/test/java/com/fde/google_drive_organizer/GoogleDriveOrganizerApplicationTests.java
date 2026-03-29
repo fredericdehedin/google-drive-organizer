9c499a1558e2036f13c372162b0e6be5b59ac596
Frederic Dehedin <frederic.dehedin@lgt.com>
2026-03-29 19:43:56 +0200

    refactor: move test properties to application.yaml

--- src/test/java/com/fde/google_drive_organizer/GoogleDriveOrganizerApplicationTests.java
+++ src/test/java/com/fde/google_drive_organizer/GoogleDriveOrganizerApplicationTests.java
@@ -2,16 +2,8 @@ package com.fde.google_drive_organizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-//TODO: move them into test ressources
-@TestPropertySource(properties = {
-		"drive.root-folder-id=test-folder-id",
-		"drive-organizer.ai.base-url=https://api.example.com/v1",
-		"drive-organizer.ai.api-key=test-key",
-		"drive-organizer.ai.model=gpt-4o"
-})
 class GoogleDriveOrganizerApplicationTests {
 
 	@Test
